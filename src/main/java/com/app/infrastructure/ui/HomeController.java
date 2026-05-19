200acf7a12ad63d9b1a53310e728f475ff19da22
Rémy <remy.thibaut2005@gmail.com>
2026-05-19 21:45:21 +0200

    Add Dashboard tab

--- src/main/java/com/app/infrastructure/ui/HomeController.java
+++ src/main/java/com/app/infrastructure/ui/HomeController.java
@@ -1,3 +1,24 @@
 package com.app.infrastructure.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
 public class HomeController {
+
+    @FXML
+    private TabPane dashboardTabPane;
+
+    @FXML
+    private Tab tabNeighbourhoods;
+
+    @FXML
+    private Tab tabEvents;
+
+    @FXML
+    private Tab tabServices;
+
+    @FXML
+    public void initialize() {
+    }
 }
